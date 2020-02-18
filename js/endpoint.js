62d6f7025ffc0cec82f51ed5e62ce9348e31f964
lemon-shark <shiqiaozhu@hotmail.com>
2020-02-18 00:15:56 -0500

    added press release

--- js/endpoint.js
+++ js/endpoint.js
@@ -71,6 +71,7 @@ function saveSubscriber() {
     }
 }
 
+
 function isValidEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
